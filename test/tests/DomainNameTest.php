4ed37700787f0701a37f684f9f0193be37315e3e
Michele Locati <michele@locati.it>
2016-10-28 21:53:02 +0200

    Add some more test to DomainName

--- test/tests/DomainNameTest.php
+++ test/tests/DomainNameTest.php
@@ -2,8 +2,11 @@
 
 namespace MLocati\IDNA\Tests;
 
+use Exception;
 use MLocati\IDNA\CodepointConverter\Utf8;
 use MLocati\IDNA\DomainName;
+use MLocati\IDNA\Exception\InvalidDomainNameCharacters;
+use MLocati\IDNA\Exception\InvalidString;
 use PHPUnit_Framework_TestCase;
 
 class DomainNameTest extends PHPUnit_Framework_TestCase
@@ -38,5 +41,33 @@ class DomainNameTest extends PHPUnit_Framework_TestCase
         $this->assertSame($deviatedName !== '', $domainName->isDeviated());
         $this->assertSame($deviatedName, $domainName->getDeviatedName());
         $this->assertSame($deviatedPunycode, $domainName->getDeviatedPunycode());
+    }
+
+    public function invalidDomainsProvider()
+    {
+        return [
+            ['email@example.com', InvalidDomainNameCharacters::class, 'The domain name contains an invalid character: @'],
+            ['http://www.domain.com', InvalidDomainNameCharacters::class, "The domain name contains 2 invalid characters:\n:\n/"],
+            ["www\0.domain.com", InvalidDomainNameCharacters::class],
+            ["\xFF", InvalidString::class],
+            ["\xFF\xFF\xFF\xFF", InvalidString::class],
+        ];
+    }
+    /**
+     * @dataProvider invalidDomainsProvider
+     */
+    public function testInvalidDomains($name, $exceptionClass, $message = null)
+    {
+        $exception = null;
+        try {
+            DomainName::fromName($name);
+        } catch (Exception $x) {
+            $exception = $x;
+        }
+        $this->assertNotNull($exception, "Domain name '$name' should not be valid");
+        $this->assertInstanceOf($exceptionClass, $exception);
+        if ($message !== null) {
+            $this->assertSame($message, $exception->getMessage());
+        }
     }
 }
