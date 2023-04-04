d6ea7800d8ab3acd05e23ca9828d68fff27e68ee
cgoedel <christoph.goedel-pavlik@helloly.com>
2023-04-04 17:37:42 +0200

    Solve problem using simpler solution suggested by the original author

--- src/Punycode.php
+++ src/Punycode.php
@@ -237,13 +237,7 @@ class Punycode
                             }
                             $code = $t + (($q - $t) % (self::BOOTSTRING_BASE - $t));
                             $result .= $dictionary[$code];
-                            if (version_compare(PHP_VERSION, '7.0') >= 0) {
-                                /** @noinspection PhpElementIsNotAvailableInCurrentPhpVersionInspection */
-                                // This prevents warning 'Deprecated: Implicit conversion from float
-                                $q = intdiv(($q - $t), (self::BOOTSTRING_BASE - $t));
-                            } else {
-                                $q = ($q - $t) / (self::BOOTSTRING_BASE - $t);
-                            }
+                            $q = intval(($q - $t) / (self::BOOTSTRING_BASE - $t));
                         }
                         $result .= $dictionary[(int) $q];
                         $bias = self::adapt($delta, $h + 1, ($h === $numBasicCodepoints));
